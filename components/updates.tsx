d1108e115df4df405638bf8b30c481545d6f6d3c
Daniel Olson <1872327+emaildano@users.noreply.github.com>
2022-02-18 18:21:05 -0500

    updated list

--- components/updates.tsx
+++ components/updates.tsx
@@ -14,14 +14,15 @@ function Post({ title, content }: PostProps) {
 }
 
 export default function Updates() {
-  const lastThirtyDays = [...new Array(30)].map((idx) =>
+  const lastThirtyDays = [...new Array(30)].map((i, idx) =>
     moment().startOf('day').subtract(idx, 'days')
   )
+
   return (
     <section className="w-full pb-6">
       <h1 className="text-center text-3xl">Status History</h1>
       {lastThirtyDays.map((item) => {
-        return <Post title={`${item}`} />
+        return <Post title={`${item._d}`} />
       })}
     </section>
   )
